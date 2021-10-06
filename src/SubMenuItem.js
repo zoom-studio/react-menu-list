zoom-studio/react-menu-list

commit c39b423e8c8dffb227f1cfc6b1a9007026bd2d7d

ensure React updates during all event handling are batched

src/SubMenuItem.js
@@ -4,7 +4,8 @@ import Kefir from 'kefir';
 import kefirBus from 'kefir-bus';
 import type {Bus} from 'kefir-bus';
 import kefirStopper from 'kefir-stopper';
-import React from 'react';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import type {Node as ReactNode} from 'react';
 import PropTypes from 'prop-types';
 import pointRectDistance from './lib/pointRectDistance';
@@ -179,7 +180,9 @@ export default class SubMenuItem extends React.Component<Props, State> {
         .takeUntilBy(this._resetMouseLeaveWatcher)
         .takeUntilBy(this._stopper)
         .onValue(() => {
-          this.open();
+          ReactDOM.unstable_batchedUpdates(() => {
+            this.open();
+          });
         });
     } else if (!highlighted) {
       this.close();
@@ -255,8 +258,10 @@ export default class SubMenuItem extends React.Component<Props, State> {
       .takeUntilBy(this._resetMouseLeaveWatcher)
       .takeUntilBy(this._stopper)
       .onValue(() => {
-        this.close();
-        menuItem.unhighlight();
+        ReactDOM.unstable_batchedUpdates(() => {
+          this.close();
+          menuItem.unhighlight();
+        });
       });
   }
 

src/MenuButton.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import FloatAnchor from 'react-float-anchor';
 import type {Options as FloatAnchorOptions} from 'react-float-anchor';
@@ -101,7 +102,9 @@ export default class MenuButton extends React.Component<Props, State> {
     ])
       .takeUntilBy(this._onClose)
       .onValue(() => {
-        this.close();
+        ReactDOM.unstable_batchedUpdates(() => {
+          this.close();
+        });
       });
 
     return new Promise(resolve => {

src/MenuList.js
@@ -1,6 +1,7 @@
 /* @flow */
 
-import React from 'react';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import type {Node as ReactNode} from 'react';
 import PropTypes from 'prop-types';
 import Kefir from 'kefir';
@@ -267,7 +268,11 @@ export default class MenuList extends React.Component<Props> {
         .filter(e => !el.contains(e.target)),
     ])
       .takeUntilBy(this._stopper)
-      .onValue(event => this._key(event));
+      .onValue(event => {
+        ReactDOM.unstable_batchedUpdates(() => {
+          this._key(event);
+        });
+      });
 
     const parentCtx = this._parentCtx();
     if (parentCtx) {

commit ad40497dd73d023cb65c0088f2865ccc555bf7b1

Fix issue #35
This issue was introduced in commit c39b423e8c8dffb227f1cfc6b1a9007026bd2d7d. SubMenuItem's code assumed that .open() would synchronously cause a full render.

src/SubMenuItem.js
@@ -317,21 +317,30 @@ export default class SubMenuItem extends React.Component<Props, State> {
               if (!this.state.opened) {
                 e.stopPropagation();
                 e.preventDefault();
-                this.open();
-                const menuInspector = this._menuInspectorRef.current;
-                if (!menuInspector) throw new Error();
-                menuInspector.moveCursor('down');
+                this.open().then(() => {
+                  const menuInspector = this._menuInspectorRef.current;
+                  if (!menuInspector)
+                    throw new Error(
+                      'SubMenuItem MenuListInspector not present'
+                    );
+                  menuInspector.moveCursor('down');
+                });
               }
             }}
             onItemChosen={(e: ChosenEvent) => {
               e.stopPropagation();
               e.preventDefault();
-              this.open();
-              if (e.byKeyboard) {
-                const menuInspector = this._menuInspectorRef.current;
-                if (!menuInspector) throw new Error();
-                menuInspector.moveCursor('down');
-              }
+              const {byKeyboard} = e;
+              this.open().then(() => {
+                if (byKeyboard) {
+                  const menuInspector = this._menuInspectorRef.current;
+                  if (!menuInspector)
+                    throw new Error(
+                      'SubMenuItem MenuListInspector not present'
+                    );
+                  menuInspector.moveCursor('down');
+                }
+              });
             }}
             aria-haspopup={true}
             aria-expanded={opened}